6b5793fa44916e7218d48036c400a1ae9a390358
pournamiazad <pournamiazad@gmail.com>
2024-08-29 23:28:23 +0530

    adding modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,33 +1,48 @@
-/* Add new recommendation */
-function addReview(event) {
-  event.preventDefault();
-  let message = document.getElementById("message").value;
-  if (!message.trim().length) {
-    alert("Please enter recommendation");
-    return false;
+document.addEventListener("DOMContentLoaded", (event) => {
+  let recommendationForm = document.getElementById("recommendation-form");
+  let backToTop = document.querySelector(".back-to-top");
+
+  /* Add new recommendation */
+  function addReview(event) {
+    event.preventDefault();
+    let message = document.getElementById("message").value;
+    let errorField = document.querySelector(".error");
+    let modal = document.querySelector(".modal");
+
+    errorField.style.display = "none";
+
+    if (!message.trim().length) {
+      errorField.style.display = "flex";
+      errorField.textContent = "Please enter recommendation";
+      return false;
+    }
+
+    modal.style.display = "flex";
   }
-  let reviewsList = document.querySelectorAll(".reviews-wrapper")[0];
 
-  const node = document.createElement("div");
-  const blockquoteNode = document.createElement("blockquote");
-  const messageNode = document.createTextNode(message);
+  // Add recommendation
+  recommendationForm.addEventListener("submit", addReview);
 
-  if (window.confirm("Do you want to submit the recommendation?")) {
+  document.getElementById("okay").addEventListener("click", () => {
+    let reviewsList = document.querySelectorAll(".reviews-wrapper")[0];
+    let message = document.getElementById("message").value;
+
+    const node = document.createElement("div");
+    const blockquoteNode = document.createElement("blockquote");
+    const messageNode = document.createTextNode(message);
     node.classList.add("review");
     blockquoteNode.appendChild(messageNode);
     node.appendChild(blockquoteNode);
     reviewsList.appendChild(node);
 
     document.getElementById("message").value = "";
-  }
-}
+    document.querySelector(".modal").style.display = "none";
+  });
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  let recommendationForm = document.getElementById("recommendation-form");
-  let backToTop = document.querySelector(".back-to-top");
-
-  // Add recommendation
-  recommendationForm.addEventListener("submit", addReview);
+  document.getElementById("cancel").addEventListener("click", () => {
+    document.querySelector(".modal").style.display = "none";
+    return false;
+  });
 
   //Back to Home
   backToTop.addEventListener("click", () => {
